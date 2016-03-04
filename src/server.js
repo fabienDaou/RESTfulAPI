829e9058d8b9390762646bc4abf7265430266df1
fabien.daoulas <fabien.daoulas@alten.nl>
2016-03-04 14:41:02 +0100

    * return resource when created

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes', (req, res) => {
         this.DBmanager.getDudes([], {mode:'ALL'})
             .then((dudes) => {
-                res.send(JSON.stringify(dudes));
+                res.send(dudes);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -46,7 +46,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes/:id', (req, res) => {
         this.DBmanager.getDudes([req.params.id])
             .then((dude) => {
-                res.send(JSON.stringify(dude));
+                res.send(dude);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -62,8 +62,8 @@ this.defineInterface = function() {
         if(req.body.email) update.Email = req.body.email;
 
         this.DBmanager.updateDude(update)
-            .then(() => {
-                res.send({updated:true});
+            .then((nbrUpdated) => {
+                res.send({updated: nbrUpdated[0]});
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -77,8 +77,9 @@ this.defineInterface = function() {
             phone: req.body.phone ? req.body.phone : '',
             email: req.body.email ? req.body.email : '',
         })
-            .then(() => {
-                console.log({created:true});
+            .then((createdDude) => {
+                console.log(createdDude);
+                res.send(createdDude.dataValues);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -103,7 +104,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes/:id/versions', (req, res) => {
         this.DBmanager.getVersions(req.params.id)
             .then((versions) => {
-                res.send(JSON.stringify(versions));
+                res.send(versions);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -113,8 +114,9 @@ this.defineInterface = function() {
 
     this.restapi.post('/rest/dudes/:id/versions', (req, res) => {
         this.DBmanager.addVersion({dudeID: req.params.id})
-            .then((success) => {
-                console.log(success);
+            .then((createdVersion) => {
+                console.log(createdVersion);
+                res.send(createdVersion.dataValues);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -128,7 +130,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes/:id/hobbies', (req, res) => {
         this.DBmanager.getHobby(req.params.id)
             .then((hobbies) => {
-                res.send(JSON.stringify(hobbies));
+                res.send(hobbies);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -139,7 +141,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes/:id/versions/:versionid/hobbies', (req, res) => {
         this.DBmanager.getHobby(req.params.id, req.params.versionid)
             .then((hobbies) => {
-                res.send(JSON.stringify(hobbies));
+                res.send(hobbies);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -154,9 +156,9 @@ this.defineInterface = function() {
         if(req.body.hobbyTitle) newHobby.hobbyTitle = req.body.hobbyTitle;
 
         this.DBmanager.addHobby(newHobby)
-            .then((success) => {
-                console.log(success);
-                res.send({success:true});
+            .then((createdHobby) => {
+                console.log(createdHobby);
+                res.send(createdHobby.dataValues);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -170,7 +172,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes/:id/jobs', (req, res) => {
         this.DBmanager.getJob(req.params.id)
             .then((jobs) => {
-                res.send(JSON.stringify(jobs));
+                res.send(jobs);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -181,7 +183,7 @@ this.defineInterface = function() {
     this.restapi.get('/rest/dudes/:id/versions/:versionid/jobs', (req, res) => {
         this.DBmanager.getJob(req.params.id, req.params.versionid)
             .then((jobs) => {
-                res.send(JSON.stringify(jobs));
+                res.send(jobs);
             })
             .catch((error) => {
                 console.log(error); // logger here
@@ -198,9 +200,9 @@ this.defineInterface = function() {
         if(req.body.location) newJob.location = req.body.location;
 
         this.DBmanager.addJob(newJob)
-            .then((success) => {
-                console.log(success);
-                res.send({success: true});
+            .then((createdJob) => {
+                console.log(createdJob);
+                res.send(createdJob.dataValues);
             })
             .catch((error) => {
                 console.log(error); // logger here
